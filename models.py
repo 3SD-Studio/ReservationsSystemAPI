d0bea1c37f65a0e3503ed355ea32fc859e7acf20
mc090 <venixis0@gmail.com>
2023-08-01 13:29:24 +0200

    0.0.1 added models.py comments

--- models.py
+++ models.py
@@ -6,16 +6,19 @@ db = SQLAlchemy()
 
 
 class Role(db.Model):
+    # Role (id, name)
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     name = sa.Column(sa.String, nullable=False)
 
 
 class Room(db.Model):
+    # Room (id, name, description, capacity, hasProjector, hasConditioning,
+    # hasTV, hasEthernet, hasWhiteboard, hasWiFi)
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     name = sa.Column(sa.String, nullable=False)
     description = sa.Column(sa.String)
     projector = sa.Column(sa.Boolean)
-    airconditioning = sa.Column(sa.Boolean)
+    conditioning = sa.Column(sa.Boolean)
     tv = sa.Column(sa.Boolean)
     ethernet = sa.Column(sa.Boolean)
     wifi = sa.Column(sa.Boolean)
@@ -23,15 +26,17 @@ class Room(db.Model):
 
 
 class User(db.Model):
+    # User (id, email, firstName, lastName, hashedPassword, role_id)
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     email = sa.Column(sa.String, nullable=False, unique=True)
     firstName = sa.Column(sa.String)
     lastName = sa.Column(sa.String)
-    password = sa.Column(sa.String) #hashed JIC
+    password = sa.Column(sa.String)  # hashed JIC
     role_id = sa.Column(sa.Integer, sa.ForeignKey(Role.id))
 
 
 class Event(db.Model):
+    # Event (id, name, description, link, start, end, owner_id)
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     name = sa.Column(sa.String, nullable=False)
     description = sa.Column(sa.String)
@@ -42,6 +47,7 @@ class Event(db.Model):
 
 
 room_event_m2m = db.Table(
+    # Reservations (room_id, event_id)
     "room_event",
     sa.Column("room_id", sa.ForeignKey(Room.id), primary_key=True),
     sa.Column("event_id", sa.ForeignKey(Event.id), primary_key=True),
@@ -49,6 +55,7 @@ room_event_m2m = db.Table(
 
 
 user_event_m2m = db.Table(
+    # Participants (event_id, user_id)
     "user_event",
     sa.Column("user_id", sa.ForeignKey(User.id), primary_key=True),
     sa.Column("event_id", sa.ForeignKey(Event.id), primary_key=True),
