44d8b2ec07326c02d05c620a732d62688e1e288e
mc090 <venixis0@gmail.com>
2023-08-09 13:55:56 +0200

    0.1 - updated User model

--- models.py
+++ models.py
@@ -71,7 +71,7 @@ class User(db.Model):
     email = sa.Column(sa.String, nullable=False, unique=True)
     firstName = sa.Column(sa.String)
     lastName = sa.Column(sa.String)
-    password = sa.Column(sa.String)  # hashed JIC
+    password = sa.Column(sa.String, nullable=False)  # hashed JIC
     role_id = sa.Column(sa.Integer, sa.ForeignKey(Role.id), default="1")
 
     def obj_to_dict(self):
