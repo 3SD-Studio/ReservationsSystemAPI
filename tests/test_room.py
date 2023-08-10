42d247ee0a90672a5d2dff4f71d8b77715ccb05a
mc090 <venixis0@gmail.com>
2023-08-10 19:08:39 +0200

    Changed routes localisation

--- tests/test_room.py
+++ tests/test_room.py
@@ -1,4 +1,4 @@
-from flask import json
+from flask import json, jsonify
 from project.models import User
 
 
@@ -7,18 +7,41 @@ def test_app(client):
     assert response.status == "200 OK"
 
 
+def test_get(client):
+    response = client.get("/events")
+    assert response.status == "200 OK"
+
+
+def test_room_post(client, app):
+    with app.app_context():
+        data = {
+           "name": "Aaa",
+           "description": "Sala konferencyjna 8",
+           "capacity": 100,
+           "projector": True,
+           "conditioning": True,
+           "tv": False,
+           "ethernet": True,
+           "wifi": True,
+           "whiteboard": False
+        }
+
+    response = client.post("/room", json=data)
+    assert response.status == "200 OK"
+
+
 def test_registration(client, app):
     data = {
-        "email": "test@test.com",
+        "email": "test2@test.com",
         "firstName": "test",
         "lastName": "test",
         "password": "test"
     }
 
-    response = client.post('/register', data=json.dumps(data))
+    response = client.post('/register', json=data)
 
     with app.app_context():
         assert response.status == "200 OK"
         assert User.query.count() == 1
-        assert User.query.first().email == "contact3sdstudio@gmail.com"
+        assert User.query.first().email == "test@test.com"
 
